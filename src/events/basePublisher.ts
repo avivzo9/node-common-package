b5c4f14a268ad40d43ec673f83d1266899baa00b
Aviv Zohar <avivzo9@gmail.com>
2024-09-27 15:52:04 +0300

    Updates

---
+++ src/events/basePublisher.ts
@@ -0,0 +1,26 @@
+import { Subjects } from "./subjects";
+
+interface Event {
+    subject: Subjects;
+    data: any;
+}
+
+export abstract class Publisher<T extends Event> {
+    abstract subject: T['subject'];
+    private client: any;
+
+    constructor(client: any) {
+        this.client = client;
+    }
+
+    publish(data: T['data']): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.client.publish(this.subject, JSON.stringify(data), (err: any) => {
+                if (err) return reject(err);
+
+                console.log('Event published to subject', this.subject);
+                resolve();
+            });
+        });
+    }
+}
